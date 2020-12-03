5867e8f6f9ba44e6e1d46ce42396b25dd5375c2a
Snorre Magnus Davøen <post@snorre.io>
2020-12-03 12:29:40 +0100

    Add day 2 solution
    Uses regex to parse each line and transforms each password
    line into a map with appropriate keys. Uses Clojure's
    frequency function to find the frequency of the letter in
    the password. Uses the get function to find letters at positions
    in the password to solve task 2.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

---
+++ 2020/solutions/day2.clj
@@ -0,0 +1,51 @@
+(ns day2
+  (:require [clojure.string :as string]
+            [clojure.edn :refer [read-string]]))
+
+(defn transform
+  "Reads numbers as numbers, and converts string to char for letter"
+  [col]
+  (-> col
+      (update 0 read-string)
+      (update 1 read-string)
+      (update 2 first)))
+
+(defn char-frequency
+  "Finds frequencies of letter in password"
+  [{:keys [pass letter] :as pass-meta}]
+  (assoc pass-meta
+         :appears
+         (get (frequencies pass) letter 0)))
+
+(defn valid-pass-1?
+  "Password checker for task 1"
+  [{:keys [min appears max]}]
+  (<= min appears max))
+
+(defn valid-pass-2?
+  "Password checker for task 2"
+  [{:keys [pass min max letter]}]
+  (->> [(get pass (dec min))
+        (get pass (dec max))]
+       (filter #(= letter %))
+       count
+       (= 1)))
+
+;; Read input and divide into sequence of numbers
+(def passwords (->> "./input/day2.txt"
+                    slurp
+                    string/split-lines
+                    (map #(->> %
+                               (re-matcher #"(\d*)-(\d*)\s([a-z]):\s([a-z]*)")
+                               re-find
+                               (drop 1)
+                               vec
+                               transform))
+                    (map (partial zipmap [:min :max :letter :pass]))
+                    (map char-frequency)))
+
+;; First task
+(count (filter valid-pass-1? passwords))
+
+;; Second task
+(count (filter valid-pass-2? passwords))
