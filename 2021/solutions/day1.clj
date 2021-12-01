cafde61a7f6e97137b9e50a223fffad598451048
Snorre Magnus Davøen <post@snorre.io>
2021-12-01 08:56:47 +0100

    Day 1 solution 2021 in clojure
    Use simple partitioning to solve problem.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

---
+++ 2021/solutions/day1.clj
@@ -0,0 +1,33 @@
+(ns day1
+  (:require [clojure.string :as string]))
+
+(def sample-input
+  "199
+200
+208
+210
+200
+207
+240
+269
+260
+263")
+
+(def input (slurp "input/day1.txt"))
+
+(->> input
+     string/split-lines
+     (map read-string)
+     (partition 2 1)
+     (filter #(> (second %) (first %)))
+     count)
+
+
+(->> input
+     string/split-lines
+     (map read-string)
+     (partition 3 1)
+     (map #(apply + %))
+     (partition 2 1)
+     (filter #(> (second %) (first %)))
+     count)
