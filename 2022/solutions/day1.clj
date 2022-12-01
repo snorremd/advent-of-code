c8d77a662c1db4cd165ef24b714d1f77c7d564cb
Snorre Magnus Davøen <post@snorre.io>
2022-12-01 11:18:05 +0100

    Day 1 solution 2022
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

---
+++ 2022/solutions/day1.clj
@@ -0,0 +1,38 @@
+(ns day1
+  (:require [clojure.string :as string]))
+
+(def sample-input
+  "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000")
+
+(def input (slurp "input/day1.txt"))
+
+
+(as-> input $
+  (string/split $ #"\n\n")
+  (map (comp #(reduce + %)
+             #(map read-string %)
+             string/split-lines) $)
+  (apply max $)
+  (println "Part 1:" $))
+
+
+(as-> input $
+  (string/split $ #"\n\n")
+  (map (comp #(reduce + %)
+             #(map read-string %)
+             string/split-lines) $)
+  (->> $ sort reverse (take 3) (apply +))
+  (println "Part 2:" $))
