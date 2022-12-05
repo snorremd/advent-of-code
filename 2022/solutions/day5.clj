3743dc6b9153d6e756bc329f10613966c5add400
Snorre Magnus Davøen <post@snorre.io>
2022-12-05 12:44:36 +0100

    Day 5 solution
    This solution required four functions over the input.
    The first function calculates the indices of the crates for each line
    based on the width of the input text. This helps us with the second
    function which parses the input to find the stacks of crates.
    
    Each stack of crates is then a sequence where the first element of the
    seq represents the top of the stack.
    
    The third function parses out the moves to do using a regex on each
    line.
    
    The final solve function takes the input reduces over the moves using
    the stacks as its accumulator and then simply shuffles the crates around
    as dictated by moves. For part 1 we simulate the 1 by 1 crate
    restriction by reverse the seq of items to move before concatinating it
    to the target stack. For the second part we simply move the list as is.
    
    Signed-off-by: Snorre Magnus Davøen <post@snorre.io>

---
+++ 2022/solutions/day5.clj
@@ -0,0 +1,53 @@
+(ns day5
+  (:require [clojure.string :as string]))
+
+(def sample-input
+  "    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2")
+
+(def input (-> (slurp "input/day5.txt")
+               #_sample-input
+               string/split-lines))
+
+(defn indices [i] ;; Find the indices of each stack for easy retrieval
+  (-> i first count (as-> $ (range 1 (inc $) 4))))
+
+(defn stacks [i]
+  (as-> i $
+    (take-while #(re-find #"\[" %) $)
+    (map #(map get (repeat %) indices) $)
+    (apply map vector $) ;; Pivot the seqs so that we get the vertical stacks
+    (mapv #(filter (partial not= \space) %) $))) ;; Remove empty spaces from stacks
+
+(defn moves [i]
+  (as-> i $
+    (drop-while #(not (re-find #"move" %)) $)
+    (map #(->> %
+               (re-matches #"move (\d+) from (\d+) to (\d+)")
+               (drop 1)
+               (map read-string)) $)))
+
+(defn solve [input move-fn]
+  (->> (reduce (fn [stacks [num from to]]
+                 (let [from-stack (nth stacks (dec from))
+                       to-stack (nth stacks (dec to))]
+                   (-> stacks
+                       (assoc (dec from) (take-last (- (count from-stack) num) from-stack))
+                       (assoc (dec to) (concat (move-fn (take num from-stack)) to-stack)))))
+               (stacks input)
+               (moves input))
+       (map first)
+       string/join))
+
+(->> (solve input reverse)
+     (println "Part 1:"))
+
+(->> (solve input identity)
+     (println "Part 2:"))
