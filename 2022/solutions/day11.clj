29ab4e1b8803bad2c31249fa1166eb9a3b23066b
Snorre Magnus Davøen <post@snorre.io>
2022-12-12 22:09:18 +0100

    Day 11 part 1 solution
    Initial work on part 2

--- 2022/solutions/day11.clj
+++ 2022/solutions/day11.clj
@@ -30,29 +30,90 @@ Monkey 3:
     If true: throw to monkey 0
     If false: throw to monkey 1")
 
+(def input (slurp "input/day11.txt"))
 
 (defn parse-monkey
   [monkey]
-  {:items (->> monkey
-               (re-find #"items: (\d+), (\d+)*")
-               rest
-               (map read-string))
+  {:id (->> monkey
+            (re-find #"Monkey (\d+):")
+            last
+            read-string)
+   :inspected 0
+   :items (->> monkey
+               string/split-lines
+               second
+               (re-seq #"(\d+)")
+               (mapv (comp read-string last)))
    :operation (->> (re-find #"Operation: new \= (old|\d+) ([\+\-\*]) (old|\d+)" monkey)
                    rest
                    (apply format "(fn [old] (%2$s %1$s %3$s))")
                    read-string
                    eval)
-   :test (let [test-fn (->> (re-find #"Test: divisible by (\d+)" monkey)
+   :relief (->> (re-find #"divisible by (\d+)" monkey)
+                last
+                read-string
+                (partial #(mod %2 %1))
+                (comp zero?))
+   :test-fn (->> (re-find #"divisible by (\d+)" monkey)
+                 last
+                 read-string
+                 (partial #(mod %2 %1))
+                 (comp zero?))
+   :test (let [test-fn (->> (re-find #"divisible by (\d+)" monkey)
                             last
                             read-string
-                            (partial mod)
+                            (partial #(mod %2 %1))
                             (comp zero?))
-               monkeys (->> (re-seq #"If true \: throw to monkey (\d+)" monkey)
+               monkeys (->> (re-seq #"throw to monkey (\d+)" monkey)
                             (map (comp read-string last)))]
-           (fn [n] (if (test-fn n)
-                     (first monkeys)
-                     (second monkeys))))})
+           (fn [n]
+             (if (test-fn n)
+               (first monkeys)
+               (second monkeys))))})
+
+(defn part-1
+  [monkeys turn]
+  (let [{:keys [items operation test]} (monkeys turn)]
+    (-> (reduce (fn [acc item]
+                  (let [item' (-> item operation (/ 3) int)]
+                    (update-in acc [(test item') :items] conj item')))
+                monkeys items)
+        (update-in [turn :inspected] (partial + (count items)))
+        (assoc-in [turn :items] []))))
+
+(defn part-2
+  [monkeys turn]
+  (let [{:keys [items operation test test-fn test-n]} (monkeys turn)]
+    (-> (reduce (fn [acc item]
+                  (let [item' (operation item)
+                        result (test-fn item)]
+                    (update-in acc [(test item') :items] conj (if result (mod ) item'))))
+                monkeys items)
+        (update-in [turn :inspected] (partial + (count items)))
+        (assoc-in [turn :items] []))))
+
+
+(as-> input $
+  (string/split $ #"\n\n")
+  (mapv parse-monkey $)
+  (iterate #(reduce part-1 % (range (count %))) $)
+  (take (inc 20) $)
+  (last $)
+  (map :inspected $)
+  (reverse (sort $))
+  (take 2 $)
+  (apply * $)
+  (println "Part 1:" $))
+
 
 (as-> sample-input $
   (string/split $ #"\n\n")
-  (map parse-monkey $))
+  (mapv parse-monkey $)
+  (iterate #(reduce part-2 % (range (count %))) $)
+  (take (inc 20) $)
+  (last $)
+  #_(map :inspected $)
+  #_(reverse (sort $))
+  #_(take 2 $)
+  #_(apply * $)
+  #_(println "Part 2:" $))
